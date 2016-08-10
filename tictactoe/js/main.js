336ca39ef575374cc8aeb57c42a9a3fb7f3b4ff4
Cam Crews <camcrews@gmail.com>
2016-08-10 12:55:33 -0400

    Starting point for TicTacToe example

---
+++ tictactoe/js/main.js
@@ -0,0 +1,18 @@
+//Objective 1
+// Let's include jQuery and this file
+// from tictactoe/index.html
+
+// Objective 2
+// Next, let's make it so that 
+// when we click on a .square,
+// we change the player whose turn it
+// is in #turn .player
+
+// Objective 3
+// Next, let's mark the square with
+// the move so that we can see who
+// went there.
+
+// Objective 4
+// Let's figure out a way to prevent
+// a user from going 
