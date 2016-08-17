7963fc65df2ab668c9fd73c153efcbdeec7799e9
Cam Crews <camcrews@gmail.com>
2016-08-17 11:39:19 -0400

    Added open and close to modal

--- modal/js/main.js
+++ modal/js/main.js
@@ -1,6 +1,37 @@
 $(document).ready(function(){
-  $("img.circle").animate({
-    right: "50px",
-    top: "200px"
-  },5000);
+  // let's test clicks on the wrapper...
+  $(".modal_wrapper").click(function(e){
+    // console.log(e.target.className);
+    if( e.target.className == "modal_wrapper") {
+      $("body").removeClass("modal-on");
+    }
+  });
+  // $("body.modal-on").click(function(){
+  //   console.log("body.modal-on...");
+  // });
+  $(".activate-modal").click(function(e){
+    e.preventDefault(); // don't follow the link
+    console.log( e );
+    $("body").addClass("modal-on");
+  });
+  $(".close").click(function(e){
+    e.preventDefault();
+    $("body").removeClass("modal-on");
+    if( $(this).hasClass("yes") ) {
+      //show animated head
+        $("img.circle").animate({
+          right: "50px",
+          top: "200px"
+        },5000);
+
+    } else if( $(this).hasClass("no") ) {  // clicked on "no" 
+      window.location = "http://rubytuesday.com";
+    } else {
+      // do nothing... user just closed dialog
+    }
+  });
+  // $(".yes").click(function(e){
+
+  // })
+
 });
