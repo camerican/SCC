ed7c4f424c5757f7154e8b27a9372f168b7f6303
Cam Crews <camcrews@gmail.com>
2016-08-18 14:56:21 -0400

    One page scrollTo effects

--- mini/one-page/js/main.js
+++ mini/one-page/js/main.js
@@ -13,4 +13,24 @@ $(document).on("ready",function(){
       $("#social_media").addClass("sticky");
     }
   });
-});
+
+  $(".link").on("click",function(e){
+    e.preventDefault();
+      var scrollTime = 500;
+      // link_home -> #section1
+      // link_about -> #section2
+      // link_contact -> #section3
+      if( $(this).hasClass("link_home") ) {
+        $.scrollTo("#section1",scrollTime,{
+          // offset: { top: -100 }
+          offset: -100
+        });
+      } else if( $(this).hasClass("link_about") ) {
+        $.scrollTo("#section2",scrollTime);
+      } else if( $(this).hasClass("link_contact") ) {
+        $.scrollTo("#section3",scrollTime);
+      }
+  });
+
+
+}); // end 
