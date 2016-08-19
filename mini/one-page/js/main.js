17575dc1da787f7fadb1b24531b228b1aad842d8
Cam Crews <camcrews@gmail.com>
2016-08-19 12:02:40 -0400

    Timed slideshow with slideFowawrd and slideBackward functions

--- mini/one-page/js/main.js
+++ mini/one-page/js/main.js
@@ -1,29 +1,80 @@
-var transition = 1;
+// var transition = 1;
+var currentSlide = 1;
+var intervalId;
+
+function goToSlideNumber(number) {
+  var selector = "#splash" + updateSlide(number); 
+  var fadeDuration = 4000;
+  $(".splash").stop().not(selector).fadeOut(fadeDuration);
+  $(selector).fadeIn(fadeDuration);
+}
+function slideForward() {
+  clearInterval(intervalId);
+  intervalId = null;
+  goToSlideNumber(currentSlide+1);
+  startCarousel();
+}
+function slideBackward() {
+  clearInterval(intervalId);
+  intervalId = null;
+  goToSlideNumber(currentSlide-1);
+  startCarousel();
+}
+// update our global variable current_slide
+// making sure that it doesn't go past the total number
+function updateSlide(number) {
+  var slideCount = document.getElementsByClassName("splash").length;
+  if( number > slideCount ) {
+    number = 1;
+  } else if( number < 1 ) {
+    number = slideCount;
+  }
+  currentSlide = number;
+  return number;
+}
+
+// Start our slideshow using setInterval
+// if intervalId is set, clear the interval
+// and reset it using setInterval again...
+function startCarousel() {
+  if( intervalId ) {
+    clearInterval(intervalId);
+  }
+  // intervalId = setInterval(function(){
+  //   goToSlideNumber(currentSlide+1);
+  // },5000);
+  intervalId = setInterval(goToSlideNumber,5000,currentSlide+1);
+}
 
 $(document).on("ready",function(){
 
+  startCarousel();
+  // intervalId = setInterval(function(){
+  //   goToSlideNumber(currentSlide+1);
+  // },5000);
+
   // jQuery carousel...  get the .splash
   // images to show in a cycle...
-  setInterval(function(){
-    var fadeDuration = 4000;
-    if( transition == 1 ) {
-      // transition from 1 to 2
-      $("#splash1").fadeOut(fadeDuration);
-      $("#splash2").fadeIn(fadeDuration);
-    } else if( transition == 2 ) {
-      // transition from 2 to 3
-      $("#splash2").fadeOut(fadeDuration);
-      $("#splash3").fadeIn(fadeDuration);
-    } else if( transition == 3 ) {
-      // transition from 3 to 1
-      $("#splash3").fadeOut(fadeDuration);
-      $("#splash1").fadeIn(fadeDuration);
-    }
-    transition += 1;
-    if( transition > 3 ) {
-      transition = 1;
-    }
-  },5000);
+  // setInterval(function(){
+  //   var fadeDuration = 4000;
+  //   if( transition == 1 ) {
+  //     // transition from 1 to 2
+  //     $("#splash1").fadeOut(fadeDuration);
+  //     $("#splash2").fadeIn(fadeDuration);
+  //   } else if( transition == 2 ) {
+  //     // transition from 2 to 3
+  //     $("#splash2").fadeOut(fadeDuration);
+  //     $("#splash3").fadeIn(fadeDuration);
+  //   } else if( transition == 3 ) {
+  //     // transition from 3 to 1
+  //     $("#splash3").fadeOut(fadeDuration);
+  //     $("#splash1").fadeIn(fadeDuration);
+  //   }
+  //   transition = transition + 1;
+  //   if( transition > 3 ) {
+  //     transition = 1;
+  //   }
+  // },5000);
 
 
 
