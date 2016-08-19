47b868356c33a3e19337158cff5f85dc84b4b105
Cam Crews <camcrews@gmail.com>
2016-08-19 12:18:02 -0400

    Forward and Backward buttons

--- mini/one-page/js/main.js
+++ mini/one-page/js/main.js
@@ -43,54 +43,19 @@ function startCarousel() {
   intervalId = setInterval(function(){
     goToSlideNumber(currentSlide+1);
   },5000);
-  // intervalId = setInterval(goToSlideNumber,5000,currentSlide+1);
 }
 
 $(document).on("ready",function(){
-
   startCarousel();
-  // intervalId = setInterval(function(){
-  //   goToSlideNumber(currentSlide+1);
-  // },5000);
-
-  // jQuery carousel...  get the .splash
-  // images to show in a cycle...
-  // setInterval(function(){
-  //   var fadeDuration = 4000;
-  //   if( transition == 1 ) {
-  //     // transition from 1 to 2
-  //     $("#splash1").fadeOut(fadeDuration);
-  //     $("#splash2").fadeIn(fadeDuration);
-  //   } else if( transition == 2 ) {
-  //     // transition from 2 to 3
-  //     $("#splash2").fadeOut(fadeDuration);
-  //     $("#splash3").fadeIn(fadeDuration);
-  //   } else if( transition == 3 ) {
-  //     // transition from 3 to 1
-  //     $("#splash3").fadeOut(fadeDuration);
-  //     $("#splash1").fadeIn(fadeDuration);
-  //   }
-  //   transition = transition + 1;
-  //   if( transition > 3 ) {
-  //     transition = 1;
-  //   }
-  // },5000);
-
-
-
-
-
+  $(".arrow-left").on("click",slideBackward);
+  $(".arrow-right").on("click",slideForward);
   $(window).on("scroll",function(){
     // Let's check to see whether we are
     // < 320px scrollY or greater...
     // we want to freeze if greater
     if( window.scrollY < 320 ) {
-      // $("#social_media").css("position","absolute");
-      // $("#social_media").css("top","400px");
       $("#social_media").removeClass("sticky");
     } else { //window.scrollY >= 320
-      // $("#social_media").css("position", "fixed");
-      // $("#social_media").css("top","80px");
       $("#social_media").addClass("sticky");
     }
     if( window.scrollY < 603 ) {
@@ -109,7 +74,6 @@ $(document).on("ready",function(){
       // link_contact -> #section3
       if( $(this).hasClass("link_home") ) {
         $.scrollTo("#section1",scrollTime,{
-          // offset: { top: -100 }
           offset: -100
         });
       } else if( $(this).hasClass("link_about") ) {
